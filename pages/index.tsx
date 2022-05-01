310d16524cdb7cad53d56161ad98d6910ef8c69a
pranay101 <pranayprajapati101@gmail.com>
2022-05-01 23:04:30 +0530

    Api integration and server side rendering enabled

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import Head from 'next/head'
 import Header from "../Components/Header/Header"
 import Nav from "../Components/Nav/Nav"
 import Results from "../Components/Results/Results"
-const Home: NextPage = () => {
+import requests from '../Utilis/requests'
+const Home: NextPage = (props) => {
+  console.log(props);
   return (
     <div className="">
       <Head>
@@ -14,6 +16,19 @@ const Home: NextPage = () => {
       <Nav />
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const genre = context.query.genre;
+  const request = await fetch(
+    `https://api.themoviedb.org/3${requests[genre]?.url}|| ${requests.fetchTrending.url}`)
+  .then(res => res.json());
+
+  return {
+    props:{
+      results:request.results,
+    },
+  };
 }
 
 export default Home

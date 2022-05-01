a2ad12b0783d7b1c9e188d0353dd2c95f00ddf19
pranay101 <pranayprajapati101@gmail.com>
2022-05-01 18:48:44 +0530

    Completed responsive header for the build

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,10 +1,31 @@
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon,
+} from "@heroicons/react/outline"
+import Image from 'next/image'
 import React from 'react'
-
+import HeaderItems from "./HeaderItems/HeaderItems"
 function Header() {
   return (
-      <header>
-          <h1>This is header</h1>
-          
+      <header className="flex flex-col sm:flex-row m-5 justify-between items-center">
+          <div className="flex flex-grow justify-evenly max-w-2xl">
+              <HeaderItems title={"HOME"} Icon={HomeIcon} />
+              <HeaderItems title={"TRENDING"} Icon={LightningBoltIcon} />
+              <HeaderItems title={"VERIFIED"} Icon={BadgeCheckIcon} />
+              <HeaderItems title={"COLLECTIONS"} Icon={CollectionIcon} />
+              <HeaderItems title={"SEARCH"} Icon={SearchIcon} />
+              <HeaderItems title={"ACCOUNT"} Icon={UserIcon} />
+          </div>
+          <Image
+                // className='object-contain'
+                src={"https://links.papareact.com/ua6"}
+                height={100}
+                width={200}
+           />
       </header>
   )
 }

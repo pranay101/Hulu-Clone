1e79bce2737364fcc58ebb1213380c0dcc8a541a
pranay101 <pranayprajapati101@gmail.com>
2022-05-01 22:43:40 +0530

    implemented navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from "../Components/Header/Header"
-
+import Nav from "../Components/Nav/Nav"
+import Results from "../Components/Results/Results"
 const Home: NextPage = () => {
   return (
     <div className="">
@@ -10,6 +11,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />  
+      <Nav />
     </div>
   )
 }
